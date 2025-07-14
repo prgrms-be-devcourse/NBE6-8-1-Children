880ae183782cc0fbda8c8dbdfacc6a5f2ab8307e
DooHyoJeong <doohj1218@naver.com>
2025-07-14 16:25:30 +0900

    create backend

---
+++ backend/src/test/java/com/gridcircle/bean_order/BeanOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gridcircle.bean_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
