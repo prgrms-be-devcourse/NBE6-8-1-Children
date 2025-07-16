2689ec45cdf6ce318795527da0a0eab1e293e2f3
yeeJin227 <dpwls99600@gmail.com>
2025-07-16 22:44:40 +0900

    feat: 중간 커밋

--- backend/src/main/java/com/gridcircle/domain/order/order/controller/OrderController.java
+++ backend/src/main/java/com/gridcircle/domain/order/order/controller/OrderController.java
@@ -5,8 +5,10 @@ import com.gridcircle.domain.order.order.dto.OrderPageResponseDto;
 import com.gridcircle.domain.order.order.dto.OrderRequestDto;
 import com.gridcircle.domain.order.order.dto.OrderResponseDto;
 import com.gridcircle.domain.order.order.service.OrderService;
+import com.gridcircle.global.security.SecurityUser;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,10 +24,11 @@ public class OrderController {
     @GetMapping("/basket/me")
     @Transactional(readOnly=true)
     @Operation(summary = "주문 페이지 데이터 조회")
-    public OrderPageResponseDto getOrderPageDataByToken(/*@AuthenticationPrincipal CustomUserDetails userDetails*/){
-        //int memberId = userDeatils.getId();
-        //return orderService.getOrderPageData(memberId);
-        return null; // 오류 때문에 임시로 null
+    public OrderPageResponseDto getOrderPageDataByToken(@AuthenticationPrincipal SecurityUser userDetails){
+        // @AuthenticationPrincipal은 현재 로그인한 사용자 정보를 메서드 파라미터로 주입해주는 어노테이션
+        // SecurityUser userDetails는 현재 로그인한 사용자 정보를 담고있는 객체
+        int memberId = userDetails.getId(); //userDetails=현재 로그인한 사용자 정보가 담긴 객체에서, id값을 얻음
+        return orderService.getOrderPageData(memberId);
     }
 
     // 주문 페이지에서 주문 등록 요청
