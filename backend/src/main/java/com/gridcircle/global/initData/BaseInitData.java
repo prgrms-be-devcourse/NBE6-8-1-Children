e9cbf7b2b4450a80d2f968409e678d4bbc490f83
YangHJ2415 <guswns2415@naver.com>
2025-07-16 21:14:59 +0900

    db에 사진 파일 구분자로 구분해 프론트에 적용

--- backend/src/main/java/com/gridcircle/global/initData/BaseInitData.java
+++ backend/src/main/java/com/gridcircle/global/initData/BaseInitData.java
@@ -51,7 +51,7 @@ public class BaseInitData {
         if (productService.count() > 0) return;
 
         Product product1 = productService.write("Natural Plants", "기본적인 원두 입니다.", "https://ifh.cc/g/xoVO1D.jpg|https://ifh.cc/g/HqQCN0.jpg|https://i.postimg.cc/zXFFRGFh/2.jpg|https://i.postimg.cc/d3TRtg2t/coffee-4591173-1920.jpg" , 5000, 20);
-        Product product2 = productService.write("Premium Beans", "프리미엄 원두 입니다.", "https://i.postimg.cc/XqvW9tKf/coffee-3392159-1920.jpg|https://ifh.cc/g/CORDgm.jpg|https://i.postimg.cc/6qwG9CSm/3.jpg|https://i.postimg.cc/G2fJRbs5/coffee-5278346-1280.jpg", 10000, 15);
+        Product product2 = productService.write("Premium Beans", "프리미엄 원두 입니다.", "https://i.postimg.cc/GhFqJQdH/11.avif|https://ifh.cc/g/CORDgm.jpg|https://i.postimg.cc/6qwG9CSm/3.jpg|https://i.postimg.cc/G2fJRbs5/coffee-5278346-1280.jpg", 10000, 15);
         Product product3 = productService.write("Special Blend", "스페셜 블렌딩 원두 입니다.", "https://i.postimg.cc/9QxPJgSF/1.jpg|https://i.postimg.cc/4dcz4f3m/2.jpg|https://i.postimg.cc/d3KGLb7n/1.jpg|https://i.postimg.cc/XqZLBrbW/farmer-6959620-1280.jpg", 15000, 10);
     }
 }
