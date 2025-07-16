01bf71d2915062e75dc86177342d79b47e7cfdca
YangHyeonJoon <guswns2415@naver.com>
2025-07-16 16:30:48 +0900

    상품 상세 페이지 기능 구현 back, 상품 상세 페이지 UI 구현 front (#34)

--- backend/src/main/java/com/gridcircle/global/initData/BaseInitData.java
+++ backend/src/main/java/com/gridcircle/global/initData/BaseInitData.java
@@ -1,7 +1,9 @@
 package com.gridcircle.global.initData;
 
 import com.gridcircle.domain.member.member.entity.Member;
+import com.gridcircle.domain.member.member.entity.Product;
 import com.gridcircle.domain.member.member.service.MemberService;
+import com.gridcircle.domain.member.member.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
@@ -17,11 +19,13 @@ public class BaseInitData {
     @Autowired
     private BaseInitData self;
     private final MemberService memberService;
+    private final ProductService productService;
 
     @Bean
     ApplicationRunner baseInitDataApplicationRunner() {
         return args -> {
             self.makeAdmin();
+            self.makeProduct();
         };
     }
 
@@ -30,5 +34,14 @@ public class BaseInitData {
         if(memberService.findByRole() > 0) return;
 
         Member member= memberService.join("admin", "관리자", "1234", "서울시 강남구 역삼동", "ROLE_ADMIN");
+    }
+
+    @Transactional
+    public void makeProduct() {
+        if (productService.count() > 0) return;
+
+        Product product1 = productService.write("Natural Plants", "기본적인 원두 입니다.", "https://ifh.cc/g/HqQCN0.jpg" , 5000, 20);
+        Product product2 = productService.write("Premium Beans", "프리미엄 원두 입니다.", "https://ifh.cc/g/CORDgm.jpg", 10000, 15);
+        Product product3 = productService.write("Special Blend", "스페셜 블렌딩 원두 입니다.", "https://ifh.cc/g/xoVO1D.jpg", 15000, 10);
     }
 }
---
+++ frontend/src/type/product.ts
@@ -0,0 +1,10 @@
+export type ProductDto = {
+    id: number;
+    productName: string;
+    description: string;
+    productImage: string;
+    price: number;
+    stock: number;
+    createdDate: string;
+    modifiedDate: string;
+}
